6471cb0c91f042a6ab94aadfc93128037e0e4e9f
pankajrajparajuli <pankajrajparajuli@gmail.com>
2026-02-09 17:58:43 +0545

    variables in js

---
+++ 01-variables/index.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+
+var greeting = "Welcome to JavaScript!";
+console.log(greeting);
+
+let number = 42;
+const pi = 3.14;
+
+console.log("Number:", number);
+console.log("Pi:", pi);
+
+number = 100;
+console.log("Updated Number:", number);
