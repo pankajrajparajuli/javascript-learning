ca9499957d82196c7817e86585f8acc08fd72645
pankajrajparajuli <pankajrajparajuli@gmail.com>
2026-02-10 15:18:48 +0545

    while & do-while

---
+++ 10-do_while(ch3)/index.js
@@ -0,0 +1,14 @@
+// While Loop in JavaScript
+let num = 10
+let i = 0;
+while(i < num) {
+    console.log("Iteration:", i);
+    i++;
+}
+
+// Do-While Loop in JavaScript
+let j = 0;
+do {
+    console.log("Do-While Iteration:", j);
+    j++;
+} while(j < num);
