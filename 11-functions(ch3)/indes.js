e381b47abe08a2387867424edf3cd40ead60584c
pankajrajparajuli <pankajrajparajuli@gmail.com>
2026-02-10 15:53:23 +0545

    function

---
+++ 11-functions(ch3)/indes.js
@@ -0,0 +1,17 @@
+function average(x, y) {
+    return (x + y) / 2;
+}
+
+let num1 = 10;
+let num2 = 20;
+
+let avg = average(num1, num2);
+console.log("Average:", avg);
+
+
+// Arrow Function
+const square = (n) =>{
+    return n * n;
+}
+
+console.log("Square of 5:", square(5));
