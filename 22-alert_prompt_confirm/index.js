b183ee3efa745adebfc5400f86900617fcb9409e
pankajrajparajuli <pankajrajparajuli@gmail.com>
2026-02-18 19:42:17 +0545

    alert prompt and confirm in js

---
+++ 22-alert_prompt_confirm/index.js
@@ -0,0 +1,8 @@
+// Alert prompt confirm
+
+alert("This is an alert message!");
+let userName = prompt("Please enter your name:");
+console.log("User Name:", userName);
+
+let isConfirmed = confirm("Do you want to proceed?");
+console.log("User Confirmation:", isConfirmed);
